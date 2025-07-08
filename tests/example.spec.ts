61b17c52079dace411073936ccf6a9659dafc56c
danny <zxcv86074@gmail.com>
2025-07-08 17:45:24 +0800

    add test case example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,16 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('密碼登入成功（切換）', async ({ page }) => {
+test('進入毛起來首頁並驗證標題', async ({ page }) => {
   await page.goto('https://www.dogcatstar.com/');
   await expect(page).toHaveTitle(/汪喵星球 DogCatStar・始終以毛孩為本/);
-
-  await page.click('#my-account-anchor', { timeout: 10000 });
-
-  await expect(page).toHaveURL('https://www.dogcatstar.com/visitor-my-account/');
-
+  await page.locator('#my-account-anchor').click();
   await expect(page).toHaveTitle(/會員中心/);
-
-  await expect(page.locator('body')).toContainText('會員中心');
-
-  await page.
-});
+})
