e14d7224526925ffd734ceb57b9626dc090f0b7c
danny <zxcv86074@gmail.com>
2025-07-11 13:04:11 +0800

    make sure browser lang is all zh_TW and userAgent has fit setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,14 +17,6 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: process.env.CI ? 'on-first-retry' : 'retain-on-failure',
-
-    // 模擬台灣使用者環境
-    locale: 'zh-TW',
-    timezoneId: 'Asia/Taipei',
-    geolocation: { longitude: 121.5654, latitude: 25.0330 },
-    permissions: ['geolocation'],
-    userAgent:
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0',
   },
 
   reporter: [
@@ -35,15 +27,36 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 } },
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+        userAgent:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        locale: 'zh-TW',
+        timezoneId: 'Asia/Taipei',
+      },
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1080 } },
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1920, height: 1080 },
+        userAgent:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0',
+        locale: 'zh-TW',
+        timezoneId: 'Asia/Taipei',
+      },
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], viewport: { width: 1920, height: 1080 } },
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1920, height: 1080 },
+        userAgent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15',
+        locale: 'zh-TW',
+        timezoneId: 'Asia/Taipei',
+      },
     },
   ],
 });
